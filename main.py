fd28a20aad3e928a286ed45ff39076ef64de984d
DevyDhanish <dhanishkariyatmain@gmail.com>
2023-11-05 14:55:33 +0530

    gan

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import numpy as np
 import random 
 from PIL import Image
+import matplotlib.pyplot as plt
 import os
 
 DATASIZE = 500
+Fsize = DATASIZE // 2
+Zsize = DATASIZE - Fsize
 XTRAIN = [ ] 
 YTRAIN = [ ]
 
 data_set_dir = "Data/"
 
 # random seleting data is not good for a neural network
-for i in range(DATASIZE):
-    ran_ch = random.choice(["0", "1"])
+for i in range(Fsize):
     ran_img = random.choice(["1.png", "2.png", "3.png", "4.png", "5.png"]) 
-    file_path = os.path.join(data_set_dir, ran_ch, ran_img)
+    file_path = os.path.join(data_set_dir, "0", ran_img)
     img = Image.open(file_path).convert("L")     #convert RGBA to gray
     img_arr = np.array(img)
     img_arr = img_arr / 255.0
     XTRAIN.append(img_arr)
-    YTRAIN.append(int(ran_ch))
+    YTRAIN.append(0)
+
+for i in range(Zsize):
+    ran_img = random.choice(["1.png", "2.png", "3.png", "4.png", "5.png"]) 
+    file_path = os.path.join(data_set_dir, "1", ran_img)
+    img = Image.open(file_path).convert("L")     #convert RGBA to gray
+    img_arr = np.array(img)
+    img_arr = img_arr / 255.0
+    XTRAIN.append(img_arr)
+    YTRAIN.append(1)
 
 class NeuralNetwork:
     def __init__( self, _inputSize, _outputSize): 
@@ -34,7 +45,7 @@ class NeuralNetwork:
         self.l2_bias = np.random.randn(5)
 
         self.l3_weights = np.random.randn(self.outputSize, 5) # 1 x 5
-        self.l3_bias = np.random.randn(1)
+        self.l3_bias = np.random.randn(self.outputSize)
 
     def sigmoid(self, x, deriv=False):
         if deriv:
@@ -49,12 +60,18 @@ class NeuralNetwork:
 
         return l1_output, l2_output, l3_output
 
-    def mse(self, predicted, actual):
-        return 1 / DATASIZE * (predicted - actual)**2
+    def binary_cross_entropy(self, predicted, actual, deriv=False):
+        if deriv:
+            epsilon = 1e-15
+            predicted = np.clip(predicted, epsilon, 1 - epsilon)
+            return (predicted - actual) / (predicted * (1 - predicted))
+        
+        epsilon = 1e-15
+        predicted = np.clip(predicted, epsilon, 1 - epsilon)
+        return - (actual * np.log(predicted) + (1 - actual) * np.log(1 - predicted))
 
-
-    def backpropagation(self, inputs, l1_out, l2_out, l3_out, error, eta):
-        delta_l3 = error * self.sigmoid(l3_out, deriv=True)
+    def backpropagation(self, inputs, l1_out, l2_out, l3_out, actual, eta):
+        delta_l3 = self.binary_cross_entropy(l3_out[0], actual, deriv=True) * self.sigmoid(l3_out, deriv=True)
         delta_l2 = np.dot(self.l3_weights.T, delta_l3) * self.sigmoid(l2_out, deriv=True) # 1 x 5 * 1, 5 x 1 * 1
         delta_l1 = np.dot(self.l2_weights.T, delta_l2) * self.sigmoid(l1_out, deriv=True)
 
@@ -68,32 +85,37 @@ class NeuralNetwork:
         self.l1_weights -= eta * np.outer(delta_l1, inputs)
         self.l1_bias -= eta * delta_l1
 
-    def train( self, epochs, eta):
+    def train( self, xset, yset, epochs, eta):
         for epoch in range(epochs):
             error = 0
             for itr in range(DATASIZE):
-                inputs = XTRAIN[itr]
-                desired = YTRAIN[itr]
+                inputs = xset[itr]
+                desired = yset[itr]
 
                 l1_out, l2_out, l3_out = self.feedForward(inputs.flatten())
                 error_raw = l3_out[0] - desired
-                error += self.mse(l3_out[0], desired)
-                self.backpropagation(inputs, l1_out, l2_out, l3_out, error_raw, eta)
+                error += self.binary_cross_entropy(l3_out[0], desired)
+                self.backpropagation(inputs, l1_out, l2_out, l3_out, desired, eta)
 
             print(f"Epoch : {epoch + 1} Error : {error / DATASIZE}")
 
+            array = self.l1_weights[0]
+            img_array = array.reshape((28,28))
+            img = Image.fromarray(img_array.astype("uint8"))
+            img.save(f"output{epoch}.jpg")
 
-model = NeuralNetwork(28 * 28, 1)
 
-model.train(1000, 0.01)
+discriminator = NeuralNetwork(28 * 28, 1)
 
-while True:
-    file_name = input("File name : ")
-    img = Image.open(file_name).convert("L")
-    img_arr = np.array(img)
-    img_arr = img_arr / 255.0
-    _, _, output = model.feedForward(img_arr.flatten())
-    print(f"Model thinks it is a : raw - {output}, rounded - {round(output[0])}")
+discriminator.train(XTRAIN, YTRAIN, 1000, 0.01)
+
+# while True:
+#     file_name = input("File name : ")
+#     img = Image.open(file_name).convert("L")
+#     img_arr = np.array(img)
+#     img_arr = img_arr / 255.0
+#     _, _, output = discriminator.feedForward(img_arr.flatten())
+#     print(f"Model thinks it is a : raw - {output}, rounded - {round(output[0])}")
 
 
 
